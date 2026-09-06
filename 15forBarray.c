15e92c861e76c084682935118925dd9eeae4c93e
jaydeepbagda13 <jaydeepbagda13@gmail.com>
2026-09-06 22:43:44 +0530

    Add program to demonstrate array element access in C

---
+++ 15forBarray.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[5] = {10, 20, 30, 40, 50};
+
+    printf("First element = %d\n", a[0]);
+    printf("Third element = %d\n", a[2]);
+    printf("Last element = %d", a[4]);
+
+    return 0;
+}
