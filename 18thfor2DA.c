707fea4abb8413c96b22f4411e7c9fdd833225f7
jaydeepbagda13 <jaydeepbagda13@gmail.com>
2026-09-07 23:40:16 +0530

    Add program to demonstrate 2D array initialization and element access in C

---
+++ 18thfor2DA.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    printf("%d\n", a[0][0]);
+    printf("%d\n", a[0][1]);
+    printf("%d\n", a[1][0]);
+    printf("%d\n", a[1][2]);
+
+    return 0;
+}
