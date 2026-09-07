e950d885df1171dfd72f1f5881a3ad3cffe8e8a7
jaydeepbagda13 <jaydeepbagda13@gmail.com>
2026-09-07 23:40:09 +0530

    Add program to demonstrate basic array initialization and access in C

---
+++ 17thforAAdd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[5] = {10, 20, 30, 40, 50};
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("a[%d] = %d\n", i, a[i]);
+    }
+
+    return 0;
+}
