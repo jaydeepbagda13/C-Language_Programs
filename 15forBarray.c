8df929798fc16e16c8103b859781f6eca113f1b5
MrJayu 13 <jaydeepbagda13@gmail.com>
2026-09-17 21:39:53 +0530

    Fix output message for second element in 15forBarray.c example program

--- 15forBarray.c
+++ 15forBarray.c
@@ -5,7 +5,7 @@ int main()
     int a[5] = {10, 20, 30, 40, 50};
 
     printf("First element = %d\n", a[0]);
-    printf("Third element = %d\n", a[2]);
+    printf("Second element = %d\n", a[1]);
     printf("Last element = %d", a[4]);
 
     return 0;
